7eb9df5b9082b0ad14fec2f3381eda11b092dcfe
anirudh201 <62521819+anirudh201@users.noreply.github.com>
2024-11-17 15:09:12 +0530

    Add files via upload
    YOU MUST RUN THIS BAT FILE TO VISIT MY WEBSITE.

---
+++ auto.bat
@@ -0,0 +1,5 @@
+git clone https://github.com/anirudh201/automaxproject.git
+cd automaxproject
+.\venv\Scripts\Activate.ps1
+pip install Django django-localflavor django-crispy-forms crispy-bootstrap4 django-filter Pillow
+python manage.py runserver 
