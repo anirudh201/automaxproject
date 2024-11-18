bfab84c16bcfbb0b4102abd43847ff3b52dab51f
anirudh201 <62521819+anirudh201@users.noreply.github.com>
2024-11-18 19:40:21 +0530

    auto.bat

--- auto.bat
+++ auto.bat
@@ -1,5 +1,7 @@
+git
 git clone https://github.com/anirudh201/automaxproject.git
 cd automaxproject
+python -m venv venv
 .\venv\Scripts\Activate.ps1
 pip install Django django-localflavor django-crispy-forms crispy-bootstrap5 django-filter Pillow
 python manage.py migrate
