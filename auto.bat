6f7de456c11e21510679f052c5d8fa0e0184a57f
anirudh201 <62521819+anirudh201@users.noreply.github.com>
2024-11-19 12:13:45 +0530

    auto.bat

--- auto.bat
+++ auto.bat
@@ -1,10 +1,15 @@
-:: download and install python
-curl -L -o python-installer.exe https://www.python.org/ftp/python/3.13.0/python-3.13.0-amd64.exe
-start /wait python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+:: Step 1: Download the Python installer
+curl -fsSL https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe -o PythonInstaller.exe
+
+:: Step 2: Install Python silently
+start /wait PythonInstaller.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+
+:: Step 3: Verify the installation
 python --version
+
 :: download and install git
 curl -fsSL https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/Git-2.43.0-64-bit.exe -o GitInstaller.exe
-start /wait GitInstaller.exe /SILENT
+start /wait GitInstaller.exe /VERYSILENT
 git --version
 :: download project from git hub
 git clone https://github.com/anirudh201/automaxproject.git
