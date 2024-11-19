edaaef5fb0c73fee6c1688eb7c5c0bb976134cb3
anirudh201 <62521819+anirudh201@users.noreply.github.com>
2024-11-19 12:14:24 +0530

    auto.bat

--- auto.bat
+++ auto.bat
@@ -1,5 +1,5 @@
 :: Step 1: Download the Python installer
-curl -fsSL https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe -o PythonInstaller.exe
+curl -fsSL https://www.python.org/ftp/python/3.13.0/python-3.13.0-amd64.exe -o PythonInstaller.exe
 
 :: Step 2: Install Python silently
 start /wait PythonInstaller.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
