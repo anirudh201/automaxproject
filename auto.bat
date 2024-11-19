330910d59270b74cd7098b04b30f9a72e6a085cc
anirudh201 <62521819+anirudh201@users.noreply.github.com>
2024-11-19 11:55:07 +0530

    auto.bat

--- auto.bat
+++ auto.bat
@@ -1,4 +1,4 @@
-curl -L -o python-installer.exe https://www.python.org/ftp/python/3.10.7/python-3.10.7-amd64.exe
+curl -L -o python-installer.exe https://www.python.org/ftp/python/3.13.0/python-3.13.0-amd64.exe
 start /wait python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 python --version
 curl -fsSL https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/Git-2.43.0-64-bit.exe -o GitInstaller.exe
