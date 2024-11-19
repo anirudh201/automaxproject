4947288b4c79340b2c0f799bef952fe899d1a358
anirudh201 <62521819+anirudh201@users.noreply.github.com>
2024-11-19 11:51:25 +0530

    auto.bat

--- auto.bat
+++ auto.bat
@@ -1,7 +1,9 @@
-@echo off
-echo Installing Git using Winget...
-winget install --id Git.Git -e --source winget
-echo Git installation completed.
+curl -L -o python-installer.exe https://www.python.org/ftp/python/3.10.7/python-3.10.7-amd64.exe
+start /wait python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+python --version
+curl -fsSL https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/Git-2.43.0-64-bit.exe -o GitInstaller.exe
+start /wait GitInstaller.exe /SILENT
+git --version
 git clone https://github.com/anirudh201/automaxproject.git
 cd automaxproject
 python -m venv venv
